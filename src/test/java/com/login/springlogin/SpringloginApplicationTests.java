3cb72d4bbbf24b36960a3385f62666ac80d0fefc
Toshimitsu Kugimoto <kugimoto1122@gmail.com>
2018-05-02 21:22:46 +0900

    Initial commit

---
+++ src/test/java/com/login/springlogin/SpringloginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.login.springlogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringloginApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
